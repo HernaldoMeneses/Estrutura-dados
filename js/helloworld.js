7d58e6d3928e5c0911195fc69f62fcfebf592644
root <root@g3telecompi.com.br>
2024-09-21 21:50:15 -0400

    var types

---
+++ js/helloworld.js
@@ -0,0 +1,20 @@
+var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+
+//Exemplo 1: Usando uma função
+
+function helloWorld() {
+    return "Hello, World!";
+}
+  
+console.log(helloWorld());
+  
+//  Exemplo 2: Utilizando um alert
+  
+alert("Hello, World!");
+  
+//  Exemplo 3: Escrevendo diretamente no HTML
+  
+//document.write("Hello, World!");
+  
---
+++ js/var.js
@@ -0,0 +1,8 @@
+var num = 1; // {1}
+num = 3; // {2}
+var price = 1.5; // {3}
+var myName = 'Packt'; // {4}
+var trueValue = true; // {5}
+var nullVar = null; // {6}
+var und; // {7}
+  
